5ae58faf149fcd55e63828a50da386a21950987a
Lautenschlager-id <tai.romani@hotmail.com>
2020-05-04 19:15:19 -0300

    add shcommands

---
+++ shcommands.js
@@ -0,0 +1,54 @@
+var loaded = false;
+var fill = false;
+
+var data = getData();
+
+var command = document.location.search.match(/[?&]cmd=(\S+)/);
+command = command ? command[1] : null;
+
+window.onload = function()
+{
+	let days = getDate(totalDays, -1, true);
+	let today = days[totalDays];
+	days.push(getDate(1)[0]);
+	data.data.labels = days;
+
+	fetch("https://discbotdb.000webhostapp.com/get?k=&f=shades_commandActivity&e=json").then((d) => d.json()).then((d) => {
+		let commands = [ [ ], [ ] ];
+
+		for (let i = 0; i <= totalDays; i++)
+			for (let j = 0; j < 2; j++)
+				if (!d[days[i]])
+					commands[j].push(0);
+				else
+				{
+					let c = 0;
+					for (n in d[days[i]][j])
+						if (!command || n == command)
+							c += d[days[i]][j][n];
+					commands[j].push(c);
+				}
+
+		data.data.datasets = [
+			{
+				label: "Game",
+				backgroundColor: hexToRGBA("#BE64C4"),
+				borderColor: "#BE64C4",
+				data: commands[0]
+			},
+			{
+				label: "Server",
+				backgroundColor: hexToRGBA("#FFC472"),
+				borderColor: "#FFC472",
+				data: commands[1]
+			}
+		];
+
+		loaded = create();
+	});
+}
+window.onkeypress = function(k){
+	if (!loaded) return;
+	if (k.keyCode == 32)
+		create(fill = !fill);
+}
